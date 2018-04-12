ae29e14e8d5089be21b2b20ca0950a39bc8ee574
fanfan <404006308@qq.com>
2018-04-13 07:12:19 +0800

    搜索
    搜索

---
+++ personalBlog/application/index/controller/Search.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+
+use app\index\controller\Base;
+class Search extends Base
+{
+    public function index()
+    {
+        $keywords=input('keywords');
+        if($keywords){
+            //按照标题搜索
+            $map['title']=['like','%'.$keywords.'%'];
+            $searchres=db('article')->where($map)->order('id desc')->paginate($listRows=3,$simple=false,$config=['query'=>array('keywords'=>$keywords)]);
+            $this->assign(array(
+                'searchres'=>$searchres,
+                'keywords'=>$keywords,
+                ));
+        }else{
+            $this->assign(array(
+                'searchres'=>null,
+                'keywords'=>'暂无数据',
+                ));
+        }
+        return view();
+    }
+}
