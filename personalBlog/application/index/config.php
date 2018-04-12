03b8c14e0003aae729a959dc767cdcce93e6f983
FAN <rfan@eduhk.hk>
2018-04-12 17:11:11 +0800

    不同种类的文章找出来显示到不同的页面

--- personalBlog/application/index/config.php
+++ personalBlog/application/index/config.php
@@ -3,6 +3,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str' => [
         '__PUBLIC__'=>SITE_URL.'/public/static/index',
+        '__IMG__'=>SITE_URL.'/public/static/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
--- personalBlog/application/index/controller/Cate.php
+++ personalBlog/application/index/controller/Cate.php
@@ -6,6 +6,9 @@ class Cate extends Base
 {
     public function index()
     {
+    	$cateid=input('cateid');
+    	$articleres=db('article')->where(array('cateid'=>$cateid))->paginate(1);
+    	$this->assign('articleres',$articleres); 
         return view();
     }
 }
