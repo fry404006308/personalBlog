07f7802bab70ae00b306b3ba323df64f192af5b9
fanfan <404006308@qq.com>
2018-04-25 05:20:04 +0800

    修复后台图标显示问题
    由绝对路径转换为相对路径

--- personalBlog/application/admin/config.php
+++ personalBlog/application/admin/config.php
@@ -12,8 +12,10 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str' => [
-        '__PUBLIC__'=>SITE_URL.'/public/static/admin',
-        '__IMG__'=>SITE_URL.'/public/static/',
+        // '__PUBLIC__'=>SITE_URL.'/public/static/admin',
+        // '__IMG__'=>SITE_URL.'/public/static/',
+        '__PUBLIC__'=>'/static/admin',
+        '__IMG__'=>'/static/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
--- personalBlog/application/index/config.php
+++ personalBlog/application/index/config.php
@@ -2,8 +2,10 @@
 return [
     // 视图输出字符串内容替换
     'view_replace_str' => [
-        '__PUBLIC__'=>SITE_URL.'/public/static/index',
-        '__IMG__'=>SITE_URL.'/public/static/',
+        // '__PUBLIC__'=>SITE_URL.'/public/static/index',
+        // '__IMG__'=>SITE_URL.'/public/static/',        
+	    '__PUBLIC__'=>'/static/index',
+        '__IMG__'=>'/static/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
